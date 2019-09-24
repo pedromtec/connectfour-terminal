76859c0d1222d20a0556ff7401694d5ad8e65012
Pedro Michael <pedromtec@gmail.com>
2019-09-24 19:58:38 -0300

    first commit

---
+++ board.py
@@ -0,0 +1,96 @@
+import time
+
+class Board:
+
+    ROWS = 6
+    COLUMNS = 7
+    COLORS = {0: '\x1b[100m', 1: '\x1b[42m', 2: '\x1b[45m'}
+    def __init__(self, player = 1):
+        self.player = player
+        self.board = [[0] * Board.COLUMNS for row in range(Board.ROWS)]
+        self.winner = None
+        self.window_winner = []
+
+    def tint (self, value):
+        if value in Board.COLORS.keys():
+             return Board.COLORS[value] + '   \x1b[0m'
+
+        return Board.COLORS[value // 3][:-1] + ';30;1;5m @ \x1b[0m'
+
+
+    def __repr__(self):
+        repr = ''
+        for row in self.board:
+            rrow = list(map(self.tint, row))
+            repr += '  '.join(rrow) + '\n\n'
+        return repr
+
+    def animation(self, col):
+        pass
+
+    def update_winner_positions(self):
+        if len(self.window_winner) <= 0:
+            return None
+        for wn in self.window_winner:
+            self.board[wn[0]][wn[1]] = self.winner * 3
+
+    def change_player(self):
+        self.player = 1 if self.player == 2 else 2
+
+    def drop_piece(self, column):
+        if self.winner:
+            return None
+        if column < 0 or column >= Board.COLUMNS:
+            return None
+        row = 0
+        while row < Board.ROWS and self.board[row][column] == 0:
+            print('\033c')
+            self.board[row][column] = self.player
+            print(self)
+            self.board[row][column] = 0
+            time.sleep(0.1)
+            row += 1
+        row-=1
+        if row < 0:
+            return None
+        self.board[row][column] = self.player
+        self.check_winner()
+        self.change_player()
+        return self
+
+    def check_window(self, window):
+        if window.count(self.player) == 4:
+            self.winner = self.player
+            return True
+        return False
+
+    def check_winner(self):
+        for row in range(Board.ROWS):
+            for col in range(Board.COLUMNS-3):
+                window = self.board[row][col: col+4]
+                if self.check_window(window):
+                    self.window_winner = [
+                        (row, col), (row, col+1), (row, col+2), (row, col+3)
+                    ]
+                    return True
+
+
+
+if __name__ == '__main__':
+    player = int(input('Quem começa? 1 ou 2 ?'))
+    board = Board(player)
+    while True:
+        print('\033c')
+        print(board, end='')
+        print(f'{Board.COLORS[board.player][:-1]};30;7;5mJogador {board.player} escolha uma coluna:\x1b[0m')
+        column = int(input())
+        if not board.drop_piece(column):
+            print('Jogada Inválida!')
+        if board.winner:
+            board.update_winner_positions()
+            break
+    print('\033c')
+    print(board, end='')
+    print('Fim do jogo!')
+    print(f'\x1b[31mO jogador vencedor foi {board.winner}\x1b[0m')
+    #print(board, end='')
